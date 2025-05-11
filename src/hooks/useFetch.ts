e56c86f6ff63e9a9507cf55653fa56b7c8112403
munergenis <munergenis@gmail.com>
2025-05-11 20:07:21 +0200

    refactor useFetch hook to add refetch capability

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 import axios from 'axios';
 
@@ -7,29 +7,30 @@ export const useFetch = <T>(endpoint: string) => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      setError(null);
-      setIsLoading(true);
+  const fetchData = useCallback(async () => {
+    setError(null);
+    setIsLoading(true);
 
-      try {
-        const res = await axios.get<T>(
-          `https://cownt-api.netlify.app/api/${endpoint}`
-        );
-        setData(res.data);
-      } catch (err) {
-        console.error(err);
-        if (err instanceof Error) {
-          setError(err.message);
-        } else {
-          setError('Unknown error');
-        }
-      } finally {
-        setIsLoading(false);
+    try {
+      const res = await axios.get<T>(
+        `https://cownt-api.netlify.app/api/${endpoint}`
+      );
+      setData(res.data);
+    } catch (err) {
+      console.error(err);
+      if (err instanceof Error) {
+        setError(err.message);
+      } else {
+        setError('Unknown error');
       }
-    };
-    fetchData();
+    } finally {
+      setIsLoading(false);
+    }
   }, [endpoint]);
 
-  return { data, isLoading, error };
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  return { data, isLoading, error, refetch: fetchData };
 };
